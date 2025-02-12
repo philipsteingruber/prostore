3cf52f0910748d5e5d03b5db661f44688dfc8de5
Philip Steingrüber <philip.steingruber@gmail.com>
2025-02-12 13:05:24 +0100

    Initial

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import "@/assets/styles/globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "ProStore",
+  description: "A modern ecommerce platform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
