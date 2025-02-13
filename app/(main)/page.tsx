8a16144ec2eef46279310886450a9b7c4ce2d407
Philip Steingrüber <philip.steingruber@gmail.com>
2025-02-13 16:21:38 +0100

    Fetch products from db

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,14 +1,22 @@
 import ProductList from "@/components/shared/product/product-list";
 import sampleData from "@/db/sample-data";
+import { getLatestProducts } from "@/lib/actions/product.actions";
 
-const Homepage = () => {
+const Homepage = async () => {
+  const latestProducts = getLatestProducts();
+  const local = false;
+
   return (
     <>
-      <ProductList
-        data={sampleData.products}
-        title="Newest Arrivals"
-        limit={4}
-      />
+      {local ? (
+        <ProductList
+          data={sampleData.products}
+          title="Newest Arrivals"
+          limit={4}
+        />
+      ) : (
+        <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
+      )}
     </>
   );
 };
---
+++ lib/actions/product.actions.ts
@@ -0,0 +1,16 @@
+"use server";
+
+import { PrismaClient } from "@prisma/client";
+import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { convertToPlainObject } from "../utils";
+
+export async function getLatestProducts() {
+  const prisma = new PrismaClient();
+
+  const data = await prisma.product.findMany({
+    take: LATEST_PRODUCTS_LIMIT,
+    orderBy: { createdAt: "desc" },
+  });
+
+  return convertToPlainObject(data);
+}
